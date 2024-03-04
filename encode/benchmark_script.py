07e6054c41eecf5bb351eca93354c4a36e9beb88
francisdbillones <francisd.billones@gmail.com>
2024-03-04 21:25:08 +0800

    refactor main.py run() function such that it is callable from other modules, and use that in benchmark_script instead of creating a new subprocess

--- encode/benchmark_script.py
+++ encode/benchmark_script.py
@@ -3,31 +3,39 @@ import subprocess
 import pathlib
 from tqdm import tqdm
 
-parser = argparse.ArgumentParser(description="Batch processing for FacePreserve")
-
-parser.add_argument("in_dir", help="Input directory")
-parser.add_argument("out_dir", help="Output directory")
-parser.add_argument(
-    "--selective_type",
-    help="Type of selective compression",
-    choices=["faceshortrange", "facelongrange", "fullbody"],
-    default="faceshortrange",
-)
-parser.add_argument(
-    "--base_crf",
-    help="CRF for non-ROI",
-    type=int,
-    default=30,
-)
-parser.add_argument(
-    "--roi_crf",
-    help="CRF for ROI",
-    type=int,
-    default=35,
-)
+from main import run
+from constants import DRAWBOX, NON_ROI_CRF, ROI_CRF
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Batch processing for FacePreserve")
+
+    parser.add_argument("in_dir", help="Input directory")
+    parser.add_argument("out_dir", help="Output directory")
+    parser.add_argument(
+        "--selective_type",
+        help="Type of selective compression",
+        choices=["faceshortrange"],  # , "facelongrange", "fullbody"],
+        default="faceshortrange",
+    )
+    parser.add_argument(
+        "--base_crf",
+        help="CRF for non-ROI",
+        type=int,
+        default=NON_ROI_CRF,
+    )
+    parser.add_argument(
+        "--roi_crf",
+        help="CRF for ROI",
+        type=int,
+        default=ROI_CRF,
+    )
+    parser.add_argument(
+        "--drawbox",
+        help="Draw bounding boxes",
+        action="store_true",
+        default=DRAWBOX,
+    )
     args = parser.parse_args()
 
     in_dir = args.in_dir
@@ -35,25 +43,21 @@ def main():
     selective_type = args.selective_type
     base_crf = args.base_crf
     roi_crf = args.roi_crf
+    drawbox = args.drawbox
 
     in_dir = pathlib.Path(in_dir)
     out_dir = pathlib.Path(out_dir)
 
     for in_filename in tqdm(in_dir.glob("*.mp4")):
         out_filename = out_dir / in_filename.name
-        subprocess.run(
-            [
-                "python3",
-                "main.py",
-                in_filename.absolute(),
-                out_filename.absolute(),
-                "--selective_type",
-                selective_type,
-                "--base_crf",
-                str(base_crf),
-                "--roi_crf",
-                str(roi_crf),
-            ]
+
+        run(
+            in_filename.absolute(),
+            out_filename.absolute(),
+            selective_type,
+            base_crf,
+            roi_crf,
+            drawbox=drawbox,
         )
 
 
--- encode/main.py
+++ encode/main.py
@@ -19,8 +19,6 @@ from utils import VideoInfo, add_rois, get_video_info
 from constants import (
     BLAZEFACE_OPTIONS,
     BLAZEFACE_UPDATE_FREQ,
-    HAARCASCADE_FACE_OPTIONS,
-    HAARCASCADE_FACE_PATH,
     HAARCASCADE_UPDATE_FREQ,
     HOG_FULLBODY_OPTIONS,
     HOG_UPDATE_FREQ,
@@ -31,33 +29,8 @@ from constants import (
     PRESET,
 )
 
-helper = lambda obj: print("DEBUG:", obj) or obj
-# helper = lambda obj: obj
-
-parser = argparse.ArgumentParser(
-    description="FacePreserve: Selective Compression for Video Calls"
-)
-parser.add_argument("in_filename", help="Input filename")
-parser.add_argument("out_filename", help="Output filename")
-# add argument --selective_type, where selective_type is either "faceshortrange", "facelongrange", or "fullbody"
-parser.add_argument(
-    "--selective_type",
-    help="Type of selective compression",
-    choices=["faceshortrange", "facelongrange", "fullbody"],
-    default="faceshortrange",
-)
-parser.add_argument(
-    "--base_crf",
-    help="CRF for non-ROI",
-    type=int,
-    default=NON_ROI_CRF,
-)
-parser.add_argument(
-    "--roi_crf",
-    help="CRF for ROI",
-    type=int,
-    default=ROI_CRF,
-)
+# helper = lambda obj: print("DEBUG:", obj) or obj
+helper = lambda obj: obj
 
 
 def ffmpeg_video_to_rgb24_process(in_filename):
@@ -77,6 +50,7 @@ def ffmpeg_add_bboxes_process(
     bboxes: List[BBox],
     update_freq: int,
     in_video_info: VideoInfo,
+    drawbox: bool = False,
 ):
     true_video_fps = in_video_info.fps
     duration_s = in_video_info.duration_s
@@ -92,6 +66,7 @@ def ffmpeg_add_bboxes_process(
                     ).filter("setpts", "PTS-STARTPTS"),
                     bboxes[helper(min(len(bboxes) - 1, i))],
                     (roi_crf - base_crf) / 51,  # calculate qoffset
+                    drawbox=drawbox,
                 )
                 for i in range(int(duration_s * true_video_fps / update_freq))
             )
@@ -124,27 +99,26 @@ def read_frame(process1: subprocess.Popen, video_info: VideoInfo):
 
 
 def run(
-    args: argparse.Namespace,
+    in_filename: str,
+    out_filename: str,
+    selective_type: str,
+    roi_crf: int,
+    base_crf: int,
+    drawbox: bool,
 ):
-    in_filename = args.in_filename
-    out_filename = args.out_filename
-    selective_type = args.selective_type
-    roi_crf = args.roi_crf
-    base_crf = args.base_crf
-
     assert roi_crf < base_crf
     assert 0 <= base_crf <= 51
     assert 0 <= roi_crf <= 51
 
-    assert selective_type in ["faceshortrange", "facelongrange", "fullbody"]
+    assert selective_type in ["faceshortrange"]  # , "facelongrange", "fullbody"]
     if selective_type == "faceshortrange":
         model = BlazeFaceDetector(BLAZEFACE_OPTIONS)
 
-    elif selective_type == "facelongrange":
-        model = HaarCascadeDetector(
-            HAARCASCADE_FACE_PATH,
-            options=HAARCASCADE_FACE_OPTIONS,
-        )
+    # elif selective_type == "facelongrange":
+    #     model = HaarCascadeDetector(
+    #         HAARCASCADE_FACE_PATH,
+    #         options=HAARCASCADE_FACE_OPTIONS,
+    #     )
 
     elif selective_type == "fullbody":
         hog_descriptor = cv2.HOGDescriptor()
@@ -179,7 +153,14 @@ def run(
                 bboxes.append(model.detect(in_frame))
 
     add_bboxes_process = ffmpeg_add_bboxes_process(
-        in_filename, out_filename, base_crf, roi_crf, bboxes, update_freq, video_info
+        in_filename,
+        out_filename,
+        base_crf,
+        roi_crf,
+        bboxes,
+        update_freq,
+        video_info,
+        drawbox,
     )
 
     video_to_rgb24_process.wait()
@@ -188,6 +169,47 @@ def run(
     add_bboxes_process.wait()
 
 
-if __name__ == "__main__":
+def main():
+    parser = argparse.ArgumentParser(
+        description="FacePreserve: Selective Compression for Video Calls"
+    )
+    parser.add_argument("in_filename", help="Input filename")
+    parser.add_argument("out_filename", help="Output filename")
+    # add argument --selective_type, where selective_type is either "faceshortrange", "facelongrange", or "fullbody"
+    parser.add_argument(
+        "--selective_type",
+        help="Type of selective compression",
+        choices=["faceshortrange"],  # , "facelongrange", "fullbody"],
+        default="faceshortrange",
+    )
+    parser.add_argument(
+        "--base_crf",
+        help="CRF for non-ROI",
+        type=int,
+        default=NON_ROI_CRF,
+    )
+    parser.add_argument(
+        "--roi_crf",
+        help="CRF for ROI",
+        type=int,
+        default=ROI_CRF,
+    )
+    parser.add_argument(
+        "--drawbox",
+        help="Draw bounding boxes",
+        action="store_true",
+        default=DRAWBOX,
+    )
+
     args = parser.parse_args()
-    run(args)
+    in_filename = args.in_filename
+    out_filename = args.out_filename
+    selective_type = args.selective_type
+    base_crf = args.base_crf
+    roi_crf = args.roi_crf
+    drawbox = args.drawbox
+    run(in_filename, out_filename, selective_type, roi_crf, base_crf, drawbox)
+
+
+if __name__ == "__main__":
+    main()
